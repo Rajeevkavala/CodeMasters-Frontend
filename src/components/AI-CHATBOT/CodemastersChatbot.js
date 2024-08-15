a29f43e9fadd2668240bde92b2d06a36ad2c967d
Rajeevkavala <68290177+Rajeevkavala@users.noreply.github.com>
2024-08-15 11:04:22 +0530

    Update CodemastersChatbot.js

--- src/components/AI-CHATBOT/CodemastersChatbot.js
+++ src/components/AI-CHATBOT/CodemastersChatbot.js
@@ -9,7 +9,7 @@ export const CodemastersChatbot = () => {
   const [showChatbox, setShowChatbox] = useState(true);
   const [thinking, setThinking] = useState(false);
 
-  const API_KEY = 'AIzaSyCiEiNZdQpogtympw4lMjg4L67PgJLm6Oc';
+  const API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
